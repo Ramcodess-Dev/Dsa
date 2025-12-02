41407a02b8f8a91b13a92a4aed448a9f74677621
Ram <kramchandraprasad7@gmail.com>
2025-12-02 09:11:35 +0530

    Time: 63 ms (71.84%) | Memory: 198.7 MB (38.68%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int countTrapezoids(vector<vector<int>>& points) {
-        const long long MOD = 1000000007;
-        unordered_map<int,long long> mp;
-        mp.reserve(points.size()*2);
-        
-        for (auto &p : points) mp[p[1]]++;
-        vector<long long> seg;
-        seg.reserve(mp.size());
+        const long long MOD = 1'000'000'007;
+        unordered_map<int,long long> freq;
 
-        for (auto &e : mp) {
+        for (auto &p : points) 
+            freq[p[1]]++;               
+
+        vector<long long> seg;
+        for (auto &e : freq) {
             long long k = e.second;
-            if (k >= 2) seg.push_back(k*(k-1)/2 % MOD);
+            if (k >= 2) 
+                seg.push_back(k * (k - 1) / 2 % MOD);   
         }
+
         long long sum = 0, ans = 0;
-        for (long long v : seg) {
-            ans = (ans + v * sum) % MOD;
-            sum = (sum + v) % MOD;
+        for (long long x : seg) {
+            ans = (ans + x * sum) % MOD;   
+            sum = (sum + x) % MOD;
         }
         return ans;
     }
